ac1778a97a45e1b029c592410a80e471df71caec
petitviolet <violethero0820@gmail.com>
2015-02-18 16:46:10 +0900

    init repo

---
+++ android_screen_shot.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+DATE_TIME=`date +"%Y%m%d-%H%M%S"`
+FILE_NAME=${DATE_TIME}.png
+
+TARGET_PATH="./"
+SIZE="x480"
+
+USAGE="
+Usage: "`basename $0`" -t [target_path] -s [size]\n
+options\n
+  -t: local file path for screen shot\n
+  -s: compress rate, using convert"
+
+while getopts ht:s: option
+do
+  case $option in
+    h)
+      echo $USAGE
+      exit 0;;
+    t)
+      TARGET_PATH=$OPTARG;;
+    s)
+      SIZE=$OPTARG;;
+    \?)
+      echo $USAGE
+      exit 1;;
+  esac
+done
+
+mkdir -p $TARGET_PATH
+adb shell screencap -p /sdcard/$FILE_NAME
+adb pull /sdcard/$FILE_NAME
+adb shell rm /sdcard/$FILE_NAME
+convert -resize $SIZE $FILE_NAME $FILE_NAME
+mv ./$FILE_NAME $TARGET_PATH
+echo screenshot saved to $TARGET_PATH/$FILE_NAME
---
+++ image_minifier.sh
@@ -0,0 +1,62 @@
+#!/bin/zsh
+
+USAGE="Usage: "`basename $0`"
+ options:
+  -t: target directory
+  -s: target size (e.g. 500x300), default 300x300
+  -v: verbose output
+  -p: prefix for resized file name, default 'mini_'
+  -f: force resize
+  -i: test output (not resize)
+  -h: show this message"
+
+# defaults
+target='.'
+size='300x300'
+prefix="mini_"
+verbose=false
+force=false
+is_test=false
+
+while getopts :t:s:p:vfih option
+do
+  case $option in
+    t)
+      target=$OPTARG;;
+    s)
+      size=$OPTARG;;
+    p)
+      prefix=$OPTARG;;
+    v)
+      verbose=true;;
+    f)
+      force=true;;
+    i)
+      is_test=true;;
+    h)
+      echo $USAGE
+      exit 0;;
+    \?)
+      echo "$0: invalid option -$OPTARG" >&2
+      echo $USAGE
+      exit 1;;
+  esac
+done
+
+if $force; then
+  size=$size!
+fi
+
+fnames=(`find $target -iregex '.*jpg' -or -iregex '.*png'`)
+
+for fname in $fnames; do
+  arr=(`echo $fname | tr -s '/' ' '`)
+  input=$fname
+  output=`echo $arr[1,$#arr-1] | tr -s ' ' '/'`'/'$prefix$arr[-1]
+  if $verbose || $is_test; then
+    echo $input '=>' $output
+  fi
+  if ! $is_test; then
+    convert -geometry $size $input $output
+  fi
+done
