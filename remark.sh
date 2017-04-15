d6dfa4b48c780e4dae69f1127c53d0e847b19b55
petitviolet <violethero0820@gmail.com>
2017-04-15 16:02:43 +0900

    remark.sh to browse md file as a presentation on Chrome

---
+++ remark.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+remark $*
+
+function remark() {
+  if [ $# -ne 1 ]; then
+    exit 1
+  fi
+  ln -s $1 /var/tmp/ &>/dev/null
+  local FILE_NAME=$(__split_file_name $1)
+  echo $FILE_NAME
+  __create_remark_html $FILE_NAME
+
+  local ORIGIN=$PWD
+  \cd /var/tmp/
+  set +m
+  if [[ $(python --version 2>&1) == *"Python 3"* ]];then
+    python -m http.server 9999 &> /dev/null &
+  else
+    python -m SimpleHTTPServer 9999 &> /dev/null &
+  fi
+  set -m
+  local PY_PID=$!
+  \cd $ORIGIN
+  open -a "Google Chrome" "http://localhost:9999/$(__remark_html_file_name $FILE_NAME)"
+  echo "*****(copied)\nkill $PY_PID\n*****"
+  echo "kill $PY_PID" | pbcopy
+}
+
+function __split_file_name() {
+  echo $1 | awk -F/ '{ print $NF }'
+}
+
+function __remark_html_file_name() {
+  local OUT=$(__split_file_name $1)
+  echo  "$OUT.html"
+}
+
+function __create_remark_html() {
+  cat << EOS | sed -e "s;{{FILE_NAME}};$1;g" > "/var/tmp/$(__remark_html_file_name $1)"
+  <DOCTYPE html>
+  <html>
+    <head><title>Presentation</title></head>
+    <body>
+      <script src="http://gnab.github.io/remark/downloads/remark-latest.min.js" type="text/javascript"></script>
+      <script type="text/javascript">
+        var slideshow = remark.create({sourceUrl: "{{FILE_NAME}}"});
+      </script>
+    </body>
+  </html>
+EOS
+}
+
