a88eaec418d9641d5557b852e4d5506525847ede
petitviolet <violethero0820@gmail.com>
2017-04-21 10:28:53 +0900

    git browse command to open remote repository on browser

---
+++ git-browse.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh -e
+
+function git_browse() {
+  local remote=$(git remote -v | ag fetch | awk '{ print $2 }' | sed 's/:/\//g' | sed 's/git@/https:\/\//g' | sed 's/.git$//g')
+  local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+  local current=$(git rev-parse --show-prefix)
+  local url=$(echo $remote/tree/$branch/$current)
+  # echo $url
+  open $url
+}
+
+git_browse
