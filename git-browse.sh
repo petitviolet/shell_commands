e650d3052095f541552ac76e31e380e6f9afce9f
petitviolet <violethero0820@gmail.com>
2017-05-10 20:44:04 +0900

    "git browse" command add support http remote

--- git-browse.sh
+++ git-browse.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh -e
 
 function git_browse() {
-  local remote=$(git remote -v | ag fetch | awk '{ print $2 }' | sed 's/:/\//g' | sed 's/git@/http:\/\//g' | sed 's/.git$//g')
+  local remote=$(git remote -v | ag fetch | awk '{ print $2 }' | sed 's;^git@\(.*\):\(.*\);http://\1\/\2;' | sed 's;^http://\(.*\)@\(.*\);http://\2;' | sed 's/.git$//g')
   local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
   local current=$(git rev-parse --show-prefix)
   local url=$(echo $remote/tree/$branch/$current)
