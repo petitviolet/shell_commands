e0b8468e03184af55427bb28999f899334d8dd2d
petitviolet <violethero0820@gmail.com>
2017-05-19 09:13:25 +0900

    add git fetch

--- git-update.sh
+++ git-update.sh
@@ -3,6 +3,7 @@
 function git_update() {
   local current_branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1 /')
   echo "updating $current_branch..."
+  git fetch
   git pull origin $current_branch --rebase --autostash
 }
 
