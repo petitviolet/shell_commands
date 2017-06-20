a109ec33cc049e048a334873def18cfc5df70acd
petitviolet <violethero0820@gmail.com>
2017-06-20 12:01:32 +0900

    fix install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for source in *.sh
+for source in $(ls | grep -v '^install')
 do
+  # remove .sh ext
   target=`echo $source | cut -d '.' -f 1`
   ln -s $(pwd)/$source /usr/local/bin/$target 2>/dev/null
   if [ $? -eq 0 ];then
