bf500c2df2ae2d6e5b853aa26a9eea49a2f53828
petitviolet <violethero0820@gmail.com>
2017-09-23 16:00:57 +0900

    removed unnecessary echo

--- git-browse.sh
+++ git-browse.sh
@@ -4,7 +4,7 @@ function git_browse() {
   local remote=$(git remote -v | ag fetch | awk '{ print $2 }' | sed 's;^git@\(.*\):\(.*\);http://\1\/\2;' | sed 's;^http://\(.*\)@\(.*\);http://\2;' | sed 's/.git$//g')
   local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
   local current=$(git rev-parse --show-prefix)
-  local url=$(echo $remote/tree/$branch/$current)
+  local url=$remote/tree/$branch/$current
   # echo $url
   open $url
 }
