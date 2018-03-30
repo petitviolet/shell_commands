45eebec48aab75236ffb41dbefd4147d8a7ab353
petitviolet <violethero0820@gmail.com>
2018-03-30 14:48:38 +0900

    enabled add target path

--- git-browse.sh
+++ git-browse.sh
@@ -3,10 +3,10 @@
 function git_browse() {
   local remote=$(git remote -v | grep fetch | awk '{ print $2 }' | sed 's;^git@\(.*\):\(.*\);http://\1\/\2;' | sed 's;^http://\(.*\)@\(.*\);http://\2;' | sed 's/.git$//g')
   local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-  local current=$(git rev-parse --show-prefix)
-  local url=$remote/tree/$branch/$current
+  local target=$(git rev-parse --show-prefix)$1
+  local url=$remote/tree/$branch/$target
   # echo $url
   open $url
 }
 
-git_browse
+git_browse $*
