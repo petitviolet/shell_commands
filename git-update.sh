48d34b72d2b7daad371b513f0ae81603ab1bb325
petitviolet <violethero0820@gmail.com>
2018-04-06 10:12:36 +0900

    modified git-update

--- git-update.sh
+++ git-update.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env sh -e
 
 function git_update() {
-  local current_branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1 /')
-  echo "updating $current_branch..."
-  git fetch
-  local r=$(git stash)
-  local EMPTY="No local changes to save"
-  git pull origin $current_branch --rebase
-  echo $r
-  if [ "$r" != "$EMPTY" ]; then
-    git stash apply stash@{0}
+  local current_branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1 /' | tr -d '[:blank:]')
+  local target_branch=$current_branch
+  if [ $# -eq 1 ]; then
+    target_branch=$1
   fi
+  echo "updating $current_branch with $target_branch..."
+  git fetch
+  git rebase -i origin/$target_branch --autostash
 }
 
-git_update
+git_update $*
