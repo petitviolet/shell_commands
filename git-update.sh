ef8401c204f02f2b38167da8fe2b2086bd6d022e
petitviolet <violethero0820@gmail.com>
2019-01-29 16:45:24 +0900

    update git-update

--- git-update.sh
+++ git-update.sh
@@ -3,7 +3,9 @@
 function git_update() {
   local current_branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1 /' | tr -d '[:blank:]')
   local target_branch=$current_branch
+  echo $target_branch
   if [ $# -eq 1 ]; then
+    echo "arg: $1"
     target_branch=$1
   fi
   echo "updating $current_branch with $target_branch..."
