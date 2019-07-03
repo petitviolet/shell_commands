78ec31bc4333cb8a262a1cd52c5a8cf79e9d30a1
petitviolet <violethero0820@gmail.com>
2019-07-03 18:26:01 +0900

    git browse open as http:// instead of ssh://

--- git-browse.sh
+++ git-browse.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh -e
 
 function git_browse() {
-  local remote=$(git remote -v | grep fetch | awk '{ print $2 }' | sed 's;^git@\(.*\):\(.*\);http://\1\/\2;' | sed 's;^http://\(.*\)@\(.*\);http://\2;' | sed 's/.git$//g')
+  local remote=$(git remote -v | grep fetch | awk '{ print $2 }' | sed 's;^ssh://;http://;' | sed 's;^git@\(.*\):\(.*\);http://\1\/\2;' | sed 's;^http://\(.*\)@\(.*\);http://\2;' | sed 's/.git$//g')
   local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
   local target=$(git rev-parse --show-prefix)$1
   local url=$remote/tree/$branch/$target
