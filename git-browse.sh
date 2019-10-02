53448c46d7f13cc338c9e0a5c27f5ab4ffe4487e
petitviolet <violethero0820@gmail.com>
2019-10-02 10:51:15 +0900

    supporeted git browse <branch> and git browse <file>

--- git-browse.sh
+++ git-browse.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh -e
 
 function git_browse() {
-  local remote=$(git remote -v | grep fetch | awk '{ print $2 }' | sed 's;^ssh://;http://;' | sed 's;^git@\(.*\):\(.*\);http://\1\/\2;' | sed 's;^http://\(.*\)@\(.*\);http://\2;' | sed 's/.git$//g')
-  local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+  local remote=$(git remote -v | grep fetch | awk '{ print $2 }' | sed 's;^ssh://;https://;' | sed 's;^git@\(.*\):\(.*\);https://\1\/\2;' | sed 's;^https://\(.*\)@\(.*\);https://\2;' | sed 's/.git$//g')
   local target=$(git rev-parse --show-prefix)$1
-  local url=$remote/tree/$branch/$target
+  if [ -e $target ]; then
+    local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+    local url=$remote/tree/$branch/$target
+  else
+    local url=$remote/tree/$1
+  fi
   # echo $url
   open $url
 }
