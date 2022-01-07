c8107181e2ad9274cb979f33802364bc4292f2ef
petitviolet <violethero0820@gmail.com>
2022-01-07 09:46:11 +0900

    updated git-browse

--- git-browse.sh
+++ git-browse.sh
@@ -2,14 +2,19 @@
 
 function git_browse() {
   local remote=$(git remote -v | grep fetch | awk '{ print $2 }' | sed 's;^ssh://;https://;' | sed 's;^git@\(.*\):\(.*\);https://\1\/\2;' | sed 's;^https://\(.*\)@\(.*\);https://\2;' | sed 's/.git$//g')
+  local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
   local target=$(git rev-parse --show-prefix)$1
-  if [ -e $target ]; then
-    local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-    local url=$remote/tree/$branch/$target
+  local url=
+  if [ -e "$target" ]; then # if file exists
+    url=$remote/tree/$branch/$target
   else
-    local url=$remote/tree/$1
+    if [ -n "$1" ]; then
+      # consider $1 a branch name
+      url=$remote/tree/$1
+    else
+      url=$remote/tree/$branch
+    fi
   fi
-  # echo $url
   open $url
 }
 
